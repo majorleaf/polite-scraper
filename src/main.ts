643d57874d971653c9d618dda90e77d0be230c1d
Madandola james <jamesmadandola@gmail.com>
2026-09-24 14:30:16 +0100

    error handling for response status

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ async function fetchPage(url : string): Promise<string> {
         "User-Agent": "FlyRankInternshipA9/1.0 (+https://github.com/majorleaf/polite-scraper)", 
     },
    });
+
+   if (response.status !== 200 ) {
+    throw new Error(`Fetch failed: ${response.status}  for ${url}`)
+   }
    const html = await response.text();
    return html;
 }
