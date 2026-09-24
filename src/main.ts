b1516ffb47b030fd85878879283578fefb8d3e3f
Madandola james <jamesmadandola@gmail.com>
2026-09-24 13:59:36 +0100

    fetch function

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
-console.log("hello scraper");
+async function fetchPage(url : string): Promise<string> { 
+   throw new Error(" not implemented");
+}
+
+fetchPage("https://books.toscrape.com/catalogue/page-1.html");
